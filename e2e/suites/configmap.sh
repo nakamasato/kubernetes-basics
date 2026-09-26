9cd8aabba525692852814db2b9611fc574525975
Naka Masato <masatonaka1989@gmail.com>
2026-09-26 16:10:51 +0900

    Revert "test: add isolated kind suites and change-aware e2e CI (#38)" (#39)
    This reverts commit d0fdec32ca2cffad131cb93dc8de75265d8f71e8.

--- e2e/suites/configmap.sh
+++
@@ -1,8 +0,0 @@
-source "${E2E_ROOT}/e2e/lib.sh"
-k apply -f 05-kubernetes-resources/05-configmap/configmap-env-var.yml
-completed test-pod
-assert_contains "$(k logs test-pod)" 'TEST_ENV=test'
-k delete pod test-pod --wait=true
-k apply -f 05-kubernetes-resources/05-configmap/configmap-file.yml
-ready test-pod
-[[ "$(k exec test-pod -- cat /datadir/data.csv)" == "$(k get cm test-file -o jsonpath='{.data.data\.csv}')" ]]
